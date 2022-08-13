cdd6e6e024a31f4126e88819d29eb5de9d8d3aed
Nikita Yashin <n.yashin88@icloud.com>
2022-08-13 15:03:50 +0300

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Nikita Yashin on 13.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
